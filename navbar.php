0b19d33116e07b3d33b4623969f5457519f94fb7
MuslimJaan <muslimmunna123@gmail.com>
2023-09-30 12:55:37 +0500

    i have created header and footer

---
+++ navbar.php
@@ -0,0 +1,46 @@
+<body>
+    <!-- nevbar start -->
+    <nav class="navbar navbar-expand-lg navbar-light py-4 my-2 bg-primary">
+        <div class="container-fluid">
+          <!-- <a class="navbar-brand" href="#">Navbar</a> -->
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto text-white mb-2 mb-lg-0 w-100 d-flex justify-content-around align-items-center">
+              <li class="nav-item">
+                <a class="nav-link active text-white fw-bold" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-white fw-bold" href="#">FEATURE</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-white fw-bold" href="#">PAGES</a>
+              
+              </li> 
+              <li class="nav-item">
+                <img src="../book-store-php/book-store/assets/images/logo_png.webp" alt="">
+              
+              </li> 
+              <li class="nav-item text-white fw-bold">
+                <a class="nav-link text-white fw-bold" href="#" >SHOP</a>
+              </li>
+              <li class="nav-item text-white fw-bold">
+                <a class="nav-link text-white fw-bold" href="#" >EVENT</a>
+              </li>
+              <li class="nav-item text-white fw-bold">
+                <a class="nav-link text-white fw-bold" href="#" >BLOG</a>
+              </li>
+              <li class="nav-item">
+                    <i class="fa fa-search mx-3 " aria-hidden="true"></i>
+    
+                    <i class="fa fa-shopping-cart mx-3" aria-hidden="true"></i>
+    
+                    <i class="fa fa-user-o mx-3" aria-hidden="true" ></i>
+    
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    <!-- nevbar end -->
