68857beba5884f5b6989e2eb1b624670d98deeab
MuslimJaan <muslimmunna123@gmail.com>
2023-10-03 09:32:14 +0500

    i have done my contant page

---
+++ book-store/home.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include "layouts/header.php" ?>
+
+<body>
+    <?php include "layouts/navbar.php" ?>
+    <hr class="mt-3">
+    <div class="container mt-2 ">
+        <div class="row">
+            <div class="col-md-3 text-center">
+                <a href="details-page/page1.html"> <img src="assets/images/product-1-800x1104.jpg" class="w-100 " alt="Image not found"></a>
+                <span class="spn fw-bold"> $38.00</span>
+                <h4 class="hov">JOHN STRASS</h4>
+                <p class="hov">Winter Darkest Tragedy</p>
+                <div class="row ">
+                    <div class="col-lg-3 ">
+                        <h6>Quantity</h6>
+                    </div>
+                    <div class="col-lg-9 ">
+                        <input class="w-100" type="number">
+                    </div>
+                </div>
+                <a href="Add-to-cart.html"> <button class="btn mt-4 m-3 bg-primary text link-light p-2 fw-bold w-50 ms-5">Add To
+                        Cart</button></a>
+            </div>
+            <div class="col-md-3 text-center ">
+                <a href="details-page/page2.html"> <img src="assets/images/product-2-600x829.jpg" alt="Image not found" class="w-100"></a>
+                <span class="spn fw-bold"> $22.00</span>
+                <h4 class="hov">JESSICA JOHNSON</h4>
+                <p class="hov">Symphony Of Trilogy</p>
+                <div class="row">
+                    <div class="col-lg-3">
+                        <h6>Quantity</h6>
+                    </div>
+                    <div class="col-lg-9 ">
+                        <input class="w-100" type="number">
+                    </div>
+                </div>
+                <a href="Add-to-cart.html"><button class="btn mt-4 m-3 bg-primary text link-light p-2 fw-bold w-50 ms-5">Add To
+                        Cart</button></a>
+            </div>
+            <div class="col-md-3 text-center ">
+                <a href="details-page/page3.html"> <img src="assets/images/product-3-600x829.jpg" alt="Image not found" class="w-100"></a>
+                <span class="spn fw-bold"> $67.00</span>
+                <h4 class="hov">JANE DOE</h4>
+                <p class="hov">Wellness And Paradise</p>
+                <div class="row">
+                    <div class="col-lg-3">
+                        <h6>Quantity</h6>
+                    </div>
+                    <div class="col-lg-9 ">
+                        <input class="w-100" type="number">
+                    </div>
+                </div>
+                <a href="Add-to-cart.html"><button class="btn mt-4 m-3  bg-primary text link-light p-2 fw-bold w-50 ms-5">Add To
+                        Cart</button></a>
+            </div>
+            <div class="col-md-3 text-center ">
+                <a href="details-page/page4.html"> <img src="assets/images/product-8-600x829.jpg" alt="Image not found" class="w-100"></a>
+                <span class="spn fw-bold"> $29.00</span>
+                <h4 class="hov">ADAM STRASS</h4>
+                <p class="hov">Fantasy Storytelling</p>
+                <div class="row">
+                    <div class="col-lg-3">
+                        <h6>Quantity</h6>
+                    </div>
+                    <div class="col-lg-9 ">
+                        <input class="w-100" type="number">
+                    </div>
+                </div>
+                <a href="Add-to-cart.html"><button class="btn mt-4 m-3 bg-primary text link-light p-2 fw-bold w-50 ms-5">Add To
+                        Cart</button></a>
+            </div>
+        </div>
+    </div>
+    </div>
+    </div>
+    <div class="container mt-5 ">
+        <div class="row" id="tableData">
+            <div class="col-md-3 text-center  ">
+                <a href="details-page/page5.html"><img src="assets/images/product-9-600x829.jpg" alt="Image not found" class="w-100"></a>
+                <span class="spn fw-bold">$78.00</span>
+                <h4 class="hov">JOHN STRASS</h4>
+                <p class="hov">Easy Fast Cooking </p>
+                <div class="row">
+                    <div class="col-lg-3">
+                        <h6>Quantity</h6>
+                    </div>
+                    <div class="col-lg-9 ">
+                        <input class="w-100" type="number">
+                    </div>
+                </div>
+                <a href="Add-to-cart.html"> <button class="btn mt-4 bg-primary m-3 text link-light p-2 fw-bold w-50 ms-5">Add To Cart</button></a>
+            </div>
+            <div class="col-md-3 text-center ">
+                <a href="details-page/page6.html"> <img src="assets/images/product-4-600x829.jpg" alt="Image not found" class="w-100"></a>
+                <span class="spn fw-bold"> $68.00</span>
+                <h4 class="hov">JAMES HOFFMAN</h4>
+                <p class="hov">Amster Hamster Trip</p>
+                <div class="row">
+                    <div class="col-lg-3">
+                        <h6>Quantity</h6>
+                    </div>
+                    <div class="col-lg-9 ">
+                        <input class="w-100" type="number">
+                    </div>
+                </div>
+                <a href="Add-to-cart.html"><button class="btn mt-4 bg-primary m-3 text link-light p-2 fw-bold w-50 ms-5">Add To Cart</button></a>
+            </div>
+            <div class="col-md-3 text-center ">
+                <a href="details-page/page7.html"><img src="assets/images/product-6-600x829.jpg" alt="Image not found" class="w-100"></a>
+                <span class="spn fw-bold"> $33.00</span>
+                <h4 class="hov">JANE DOE </h4>
+                <p class="hov">Summertime Love Stories</p>
+                <div class="row">
+                    <div class="col-lg-3">
+                        <h6>Quantity</h6>
+                    </div>
+                    <div class="col-lg-9 ">
+                        <input class="w-100" type="number">
+                    </div>
+                </div>
+                <a href="Add-to-cart.html"><button class="btn mt-4 bg-primary m-3 text link-light p-2 fw-bold w-50 ms-5">Add To Cart</button></a>
+            </div>
+            <div class="col-md-3 text-center ">
+                <a href="details-page/page8.html"><img src="assets/images/product-10-600x829.jpg" alt="Image not found" class="w-100"></a>
+                <del>$24.00</del> <span class="spn fw-bold"> $56.00</span>
+                <h4 class="hov">JOHN STRASS</h4>
+                <p class="hov">Japan Travel Stories</p>
+                <div class="row">
+                    <div class="col-lg-3">
+                        <h6>Quantity</h6>
+                    </div>
+                    <div class="col-lg-9 ">
+                        <input class="w-100" type="number">
+                    </div>
+                </div>
+                <a href="Add-to-cart.html"><button class="btn mt-4 bg-primary m-3 text link-light p-2 fw-bold w-50 ms-5">Add To Cart</button></a>
+            </div>
+
+        </div>
+    </div>
+    </div>
+    </div>
+    <?php include "layouts/footer.php" ?>
+</body>
+
+</html>
